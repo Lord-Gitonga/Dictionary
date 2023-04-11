6adb33e43ae3ec5e87c68c9c71093189ba7d3e8b
lordish <gitonga403@gmail.com>
2023-04-11 13:51:55 +0300

    Look-up any word

---
+++ index.js
@@ -0,0 +1,42 @@
+const dictionaryUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById('result')
+//const sound = document.getElementById('sound')
+const btn = document.getElementById('search-btn')
+
+btn.addEventListener('click', ()=>{
+    let inputWord = document.getElementById('input-word').value;
+     fetch(`${dictionaryUrl}${inputWord}`)
+     .then(resp=>resp.json())
+     
+     .then(data=>{
+        //console.log(data)
+         result.innerHTML = `
+         <div class="word">
+                <h3>${inputWord}</h3>
+                <button">
+                    <i class="fa-solid fa-music"></i>
+                </button>
+            </div>
+            <div class="details">
+                <p>${data[0].meanings[0].partOfSpeech}</p>
+                <p>/${data[0].phonetics[1].text}/</p>
+            </div>
+            <p class="word-meaning">
+             ${data[0].meanings[0].definitions[0].definition}
+            <p class="word-example">
+            ${data[0].meanings[0].synonyms}
+            </p>
+         `; 
+         //sound.setAttribute("src", `https:${data[0].phonetics[1].audio}`);
+         //console.log(sound);
+     })
+
+     .catch(()=>{
+        result.innerHTML = `<h3 class="error">Couldn\'t Find The word</h3>`
+     });
+})
+
+// function playSound(){
+//     sound.play();
+// }
+
